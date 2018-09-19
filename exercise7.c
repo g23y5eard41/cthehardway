45272fa64b9663cc7567946f8860ef3a2fa40342
g23y5eard41 <g23y5eard41@blackdogisr.com>
2018-09-19 08:50:12 -0500

    initial commit

---
+++ exercise7.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(int argc, char*argv[])
+{
+    int distance = 100;
+    float power = 2.345f;
+    double super_power = 56789.4532;
+    char initial = 'B';
+    char first_name[] = "William";
+    char last_name[] = "Taylor";
+
+    printf("You are %d miles away.\n", distance);
+    
+
+
+
+
+
+
+    return 0;
+}
